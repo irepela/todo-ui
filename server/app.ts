17aca0e1b71920ccf339a5320b72eff4464302ba
irepela <irepela@softserveinc.com>
2018-01-16 11:56:21 +0200

    Fixed server side code to correctly return static

--- server/app.ts
+++ server/app.ts
@@ -1,10 +1,15 @@
-import Koa from 'koa';
-const app = new Koa();
+import koa from 'koa';
+import * as path from 'path';
+import serveStatic from 'koa-static';
+import compress from 'koa-compress';
+import logger from 'koa-logger';
+const app = new koa();
 
-// response
-app.use(async (ctx) => {
-  ctx.body = 'Hello World';
-});
+app.use(logger());
+app.use(compress());
+
+const root = path.resolve(__dirname, '../');
+app.use(serveStatic(path.resolve(root, 'todo-list')));
 
 app.listen(3000, () => console.log('server started 3000'));
 
